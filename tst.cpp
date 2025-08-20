db2bda0a0cb07f2473c40819aec0fbb11f1b6212
Mahmoud Ghonem <atefghonem10@gmail.com>
2025-08-20 12:12:50 +0300

    #Ghonem
    Modifing the test file

--- tst.cpp
+++ tst.cpp
@@ -1,13 +1,4 @@
 #include <bits/stdc++.h>
-#define Test int t; cin>>t;while(t--)
-#define ll   long long
-#define yes  cout <<"YES\n";
-#define no   cout<<"NO\n";
-#define L    <<'\n';
-#define For  for (int i =0 ;i<n;i++)
-#define all(v)  v.begin(),v.end()
-#define YARAB_ACCEPTED ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
 using namespace std;
 
 
